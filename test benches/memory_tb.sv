d1f94c589f9a225c7034bdb2193eb24601d658e3
AmanMurad <aman.murad6600@gmail.com>
2024-05-30 22:36:06 +0500

    files updated

--- test benches/memory_tb.sv
+++ test benches/memory_tb.sv
@@ -1,3 +1,12 @@
+// Copyright 2023 University of Engineering and Technology Lahore.
+// Licensed under the Apache License, Version 2.0, see LICENSE file for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Description:  
+//
+// Author: Aman Murad, UET Lahore
+// Date: 30.5.2024
+
 `timescale 1ns / 1ps
 
 `include "../rtl/defines/mem_defs.svh"
