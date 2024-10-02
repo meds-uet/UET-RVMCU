d6f163d548063755290a2e21d975b269b6048006
AmanMurad <aman.murad6600@gmail.com>
2024-10-02 14:38:30 +0500

    sdk files updated according to integration

--- sdk/make.bat
+++ sdk/make.bat
@@ -1,11 +1,14 @@
 rmdir -r build
 mkdir build
 
-riscv64-unknown-elf-as -c -o build/startup.o src/startup.s -march=rv32ima -mabi=ilp32
+riscv64-unknown-elf-as -c -o startup.o src/startup.s -march=rv32ima -mabi=ilp32
 
-riscv64-unknown-elf-gcc -c -o build/main.o src/main.c -march=rv32im -mabi=ilp32
+riscv64-unknown-elf-gcc -c -O -o build/main.o src/main.c -march=rv32im -mabi=ilp32
+riscv64-unknown-elf-gcc -c -O -o build/uart.o interfaces/uart.c -march=rv32im -mabi=ilp32
+riscv64-unknown-elf-gcc -c -O -o build/spi.o interfaces/spi.c -march=rv32im -mabi=ilp32
+riscv64-unknown-elf-gcc -c -O -o build/gpio.o interfaces/gpio.c -march=rv32im -mabi=ilp32
 
-riscv64-unknown-elf-gcc -O -o build/program.elf build/startup.o build/main.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-gcc -O -o build/program.elf build/main.o build/uart.o build/spi.o build/plic.o build/gpio.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
 
 riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/program.elf build/main.bin
 
